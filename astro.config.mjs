7e6cc12afca411fe9567e57dfa39fd73325da34b
marteyhaw <martey.haw@gmail.com>
2024-02-12 21:56:30 -0800

    Add an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://main--gilded-chaja-5f4cb6.netlify.app/',
+});
